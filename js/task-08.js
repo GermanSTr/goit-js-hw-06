89e55f6ac373da4c0338a4ca0f3f804f62ce2b6f
GermanSTr <gershev1994@gmail.com>
2023-10-09 18:28:45 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formClient = document.querySelector(".login-form");
 
+formClient.addEventListener("submit", correctForm);
+
+function correctForm(element) {
+  element.preventDefault();
+  //   const form = element.target;
+  const emailForm = element.target.elements.email.value;
+  const passwordForm = element.target.elements.password.value;
+  if (emailForm === "" || passwordForm === "") {
+    alert("WARNING! The Email and Password fields must be filled.");
+  } else {
+    const result = {};
+    result.email = emailForm;
+    result.password = passwordForm;
+    console.log(result);
+  }
+  element.target.reset();
+}
