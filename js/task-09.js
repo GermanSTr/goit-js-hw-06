f0d9035dbf229a428c5a5d0c17e25f3885b83cb0
GermanSTr <gershev1994@gmail.com>
2023-10-10 00:41:15 +0300

    1

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
+const widGet = document.querySelector(".widget");
+const changeColor = widGet.querySelector(".change-color");
+
+changeColor.addEventListener("click", colorElement);
+
+function colorElement() {}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
