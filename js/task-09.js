909824a69a47695147ef6136b8d4567ad103b202
GermanSTr <gershev1994@gmail.com>
2023-10-10 16:57:28 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,9 +1,19 @@
-const widGet = document.querySelector(".widget");
-const changeColor = widGet.querySelector(".change-color");
+const widGetP = document.querySelector(".widget p");
+const widGetSpan = document.querySelector(".widget span");
+const widGetButton = document.querySelector(".widget button");
+const elementsColor = [widGetP, widGetSpan, widGetButton];
+const changeColor = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
 
 changeColor.addEventListener("click", colorElement);
 
-function colorElement() {}
+function colorElement() {
+  const result = getRandomHexColor();
+  elementsColor.forEach((element) => {
+    element.style.backgroundColor = result;
+  });
+  colorName.textContent = result;
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
